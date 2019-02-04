3254bde962deb9e35276bc9e83df98926338d806
defoliation <defoli_ation@outlook.com>
2019-02-04 18:52:52 +0800

    SerializationHelper done.

--- src/main/java/net/codevmc/extraItems/item/serialization/SerializationHelper.java
+++ src/main/java/net/codevmc/extraItems/item/serialization/SerializationHelper.java
@@ -1,5 +1,7 @@
 package net.codevmc.extraItems.item.serialization;
 
+import com.google.common.primitives.Bytes;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.*;
@@ -7,26 +9,22 @@ import java.util.*;
 public class SerializationHelper {
 
     public static String serialize(Object o) {
-//        StringBuilder sb = new StringBuilder();
-//        for(String s : getSerializeField(o))
-//            sb.append(s+"¤");
-//        sb.substring(0,sb.length()-1);
-        return getSerializeField(o).toString();
+        return getBasicDataTypeSymbol(o) + getSerializeObject(o).toString();
     }
 
-    private static List<String> getSerializeField(Object o) {
+    private static List<String> getSerializeObject(Object o) {
         List<String> serializeFieldList = new ArrayList<>();
         Arrays.asList(o.getClass().getDeclaredFields())
                 .stream()
                 .peek(field -> field.setAccessible(true))
                 .filter(field -> hasSerializationAnnotation(field))
                 .forEach(field -> {
-                    serializeFieldList.add(serializeClass(o, field));
+                    serializeFieldList.add(serializeField(o, field));
                 });
         return serializeFieldList;
     }
 
-    private static String serializeClass(Object o, Field field) {
+    private static String serializeField(Object o, Field field) {
         Class type = field.getType();
         String fieldClassName = type.getName();
         String fieldName = field.getName();
@@ -59,7 +57,7 @@ public class SerializationHelper {
                 case "float":
                     return fieldName + ":" + symbolName + field.getFloat(o);
                 case "java.lang.String":
-                    return fieldName + ":" + symbolName + "[" + field.get(o) + "]";
+                    return fieldName + ":" + symbolName + Arrays.toString(((String) field.get(o)).getBytes());
                 case "Collection":
                     return fieldName + ":" + symbolName + serializeCollection((Collection) field.get(o));
                 case "Map":
@@ -97,7 +95,7 @@ public class SerializationHelper {
             case "float":
                 return getBasicDataTypeSymbol(o) + o.toString();
             case "java.lang.String":
-                return getBasicDataTypeSymbol(o) + "[" + o.toString() + "]";
+                return getBasicDataTypeSymbol(o) + Arrays.toString(((String) o).getBytes());
             case "Collection":
                 return getBasicDataTypeSymbol(o) + serializeCollection((Collection) o);
             case "Map":
@@ -137,9 +135,9 @@ public class SerializationHelper {
             case "java.lang.String":
                 return "T";
             case "Collection":
-                return "O"+clazz.getName();
+                return "O" + clazz.getName();
             case "Map":
-                return "P"+clazz.getName();
+                return "P" + clazz.getName();
             default:
                 return "L" + name;
         }
@@ -174,22 +172,42 @@ public class SerializationHelper {
         return false;
     }
 
-    public static <T> T deserialize(T t, String str) {
+    public static <T> T deserialize(String str) {
         str = str.trim();
         if (str.isEmpty())
-            return t;
-
-
-        return null;
+            return null;
+        return (T) deserializeField(str);
     }
 
-    private static Map<String, Object> deserialize2Map(String str) {
-        HashMap<String, Object> map = new HashMap<>();
-
-        return null;
+    private static Map<String, Object> deserializeMap(String str) {
+        Map<String, Object> map;
+        try{
+            map = (Map<String, Object>) getInstanceObjectByClassName(getClassNameFromSerializeString(str));
+        }catch (Exception e){
+            map = new HashMap<>();
+        }
+        MapDeserializer deserializer = new MapDeserializer(getElementFromSerializeString(str));
+        while (deserializer.hasNextEntry()) {
+            Map.Entry<String, Object> entry = deserializer.getNextEntry();
+            map.put(entry.getKey(), entry.getValue());
+        }
+        return map;
     }
 
-    private static class Serializer {
+    private static String getClassNameFromSerializeString(String str) {
+        return str.substring(0, str.indexOf('['));
+    }
+
+    private static Object getInstanceObjectByClassName(String str) {
+        try {
+            Class clazz = Class.forName(str);
+            return clazz.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+        }
+        throw new RuntimeException(str);
+    }
+
+    private static class MapDeserializer {
 
         private String string;
 
@@ -197,76 +215,47 @@ public class SerializationHelper {
 
         private int lastIndex = 0;
 
-        public Serializer(String string) {
+        boolean indexByHasNextEntry = false;
+
+        public MapDeserializer(String string) {
             if (string.startsWith("[") && string.endsWith("]"))
                 this.string = string.substring(1, string.length() - 1);
             else throw new RuntimeException("wrong serialized string: " + string);
         }
 
         public Map.Entry<String, Object> getNextEntry() {
-            indexNextComma();
+            if (!indexByHasNextEntry)
+                indexNextComma();
+            indexByHasNextEntry = false;
             String piece = string.substring(lastIndex, index).trim();
             String key = piece.substring(0, piece.indexOf(':'));
             String value = piece.substring(piece.indexOf(':') + 1);
+            Object o = deserializeField(value);
+            return new Map.Entry<String, Object>() {
+                @Override
+                public String getKey() {
+                    return key;
+                }
 
-            return null;
-        }
+                @Override
+                public Object getValue() {
+                    return o;
+                }
 
-        private Object deserialize(String str) {
-            char startChar = str.charAt(0);
-            if(startChar=='N')
-                return null;
-            String serializeString = str.substring(1);
-            switch (startChar) {
-                case 'I':
-                    return Integer.valueOf(serializeString);
-                case 'J':
-                    return Long.valueOf(serializeString);
-                case 'S':
-                    return Short.valueOf(serializeString);
-                case 'Z':
-                    return Boolean.valueOf(serializeString);
-                case 'B':
-                    return Byte.valueOf(serializeString);
-                case 'C':
-                    return Character.valueOf(serializeString.charAt(0));
-                case 'D':
-                    return Double.valueOf(serializeString);
-                case 'F':
-                    return Float.valueOf(serializeString);
-                case 'T':
-                    return serializeString.substring(1,serializeString.length()-1);
-                case 'O':
-                    String className = serializeString.substring(0,serializeString.indexOf('['));
-                    String serializeElements = serializeString.substring(serializeString.indexOf('[')+1,serializeString.indexOf(']'));
-                    try {
-                        Class collectionClass = Class.forName(className);
-                        Collection collection = (Collection) collectionClass.newInstance();
-
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                case 'P':
-                    return null;
-                case 'L':
-                    return null;
-                default:
-                    throw new RuntimeException("deserialize error: "+str);
-            }
+                @Override
+                public Object setValue(Object value) {
+                    return o;
+                }
+            };
         }
 
         public boolean hasNextEntry() {
-            int before = index;
+            lastIndex = index;
             indexNextComma();
             if (index == lastIndex) {
                 return false;
             }
-            index = before;
+            indexByHasNextEntry = true;
             return true;
         }
 
@@ -283,5 +272,135 @@ public class SerializationHelper {
                 index++;
             }
         }
+    }
+
+    private static String getElementFromSerializeString(String str) {
+        return str.substring(str.indexOf('['));
+    }
+
+    private static Collection deserializeCollection(String str) {
+        Collection collection = (Collection) getInstanceObjectByClassName(getClassNameFromSerializeString(str));
+        String[] elementArray = getElementArray(str);
+        for (String element : elementArray)
+            collection.add(deserializeField(element));
+        return collection;
+    }
+
+    private static String[] getElementArray(String str) {
+        String elementsWithBracket = getElementFromSerializeString(str);
+        String elements = elementsWithBracket.substring(1, elementsWithBracket.length() - 1);
+        return elements.split(",");
+    }
+
+    private static String deserializeString(String bytes) {
+        ArrayList<Byte> byteArrayList = new ArrayList<>();
+        for (String b : getElementArray(bytes))
+            byteArrayList.add(Byte.valueOf(b));
+        return new String(Bytes.toArray(byteArrayList));
+    }
+
+    private static Object deserializeField(String element) {
+        char startChar = element.charAt(0);
+        if (startChar == 'N')
+            return null;
+        String serializeString = element.substring(1);
+        switch (startChar) {
+            case 'I':
+                return Integer.valueOf(serializeString);
+            case 'J':
+                return Long.valueOf(serializeString);
+            case 'S':
+                return Short.valueOf(serializeString);
+            case 'Z':
+                return Boolean.valueOf(serializeString);
+            case 'B':
+                return Byte.valueOf(serializeString);
+            case 'C':
+                return Character.valueOf(serializeString.charAt(0));
+            case 'D':
+                return Double.valueOf(serializeString);
+            case 'F':
+                return Float.valueOf(serializeString);
+            case 'T':
+                return deserializeString(serializeString);
+            case 'O':
+                return deserializeCollection(serializeString);
+            case 'P':
+                return deserializeMap(serializeString);
+            case 'L':
+                return deserializeObject(serializeString);
+            default:
+                throw new RuntimeException("deserializeField error: " + element);
+        }
+    }
+
+    private static Object deserializeObject(String s) {
+        try {
+            return deserializeObject(Class.forName(getClassNameFromSerializeString(s)), getElementFromSerializeString(s));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+
+    private static Object deserializeObject(Class clazz, String s) {
+        try {
+            return deserializeObject(clazz.newInstance(), s);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+
+    private static Object deserializeObject(Object instance, String s) {
+        Map<String, Object> fieldMap = deserializeMap(s);
+        Class clazz = instance.getClass();
+        for (Map.Entry<String, Object> entry : fieldMap.entrySet()) {
+            String fieldName = entry.getKey();
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                String fieldClassName = field.getType().getName();
+                try {
+                    switch (fieldClassName) {
+                        case "int":
+                            field.setInt(instance, (int) entry.getValue());
+                            break;
+                        case "long":
+                            field.setLong(instance, (long) entry.getValue());
+                            break;
+                        case "short":
+                            field.setShort(instance, (short) entry.getValue());
+                            break;
+                        case "boolean":
+                            field.setBoolean(instance, (boolean) entry.getValue());
+                            break;
+                        case "byte":
+                            field.setByte(instance, (byte) entry.getValue());
+                            break;
+                        case "char":
+                            field.setChar(instance, (char) entry.getValue());
+                            break;
+                        case "double":
+                            field.setDouble(instance, (double) entry.getValue());
+                            break;
+                        case "float":
+                            field.setFloat(instance, (float) entry.getValue());
+                            break;
+                        default: {
+                            field.set(instance,entry.getValue());
+                        }
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return instance;
     }
 }
